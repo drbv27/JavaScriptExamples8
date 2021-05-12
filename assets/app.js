bfcd27909dfe55a79f377a4bf6ee572d9e88c00b
Diego Bonilla <drbv27@gmail.com>
2021-05-12 17:01:48 -0500

    Final solutions

--- assets/app.js
+++ assets/app.js
@@ -12,6 +12,38 @@
 console.log("-----------------------------------------------------------------------------------")
 console.log("Ejercicio 24:")
 
+const arrAscDes = (arr) => {
+    
+    let acs = [...arr]
+    let dsc = [...arr]
+    const order = {}
+
+    return console.info({
+        acs : acs.sort(function(a, b) {
+            return a - b}),
+        dsc : dsc.sort(function(a, b) {
+            return b - a})
+    })
+}
+arrAscDes([91,20,2,87,5,17,48])
+
+//E24.S2 validation map sort and reverse
+
+const ordenarArreglo = (arr=undefined) =>{
+    if (arr===undefined) return console.warn('No ingesaste un arreglo')
+    if (!(arr instanceof Array)) return console.error('El valos que ingresaste no es un arreglo')
+    if (arr.length===0) return console.error('No puedes ingresar un arreglo vacio')
+    for (let num of arr) {
+        if(typeof num !== "number") return console.error('El arreglos solo puede contener numeros')
+    }
+
+    return console.info({
+        arr,
+        asc: arr.map(el =>el).sort(),
+        desc: arr.map(el =>el).sort().reverse()
+    })
+}
+ordenarArreglo([34,21,52,17,99,18])
 
 
 //--------------------------------------------------------------------------------------------
@@ -20,7 +52,32 @@ console.log("Ejercicio 24:")
 console.log("-----------------------------------------------------------------------------------")
 console.log("Ejercicio 25:")
 
+const qutarDuplicados = (arr=undefined) => {
+    if (arr===undefined) return console.warn('No ingesaste un arreglo')
+    if (!(arr instanceof Array)) return console.error('El valos que ingresaste no es un arreglo')
+    if (arr.length===0) return console.error('No puedes ingresar un arreglo vacio')
+    if (arr.length===1) return console.error('el arreglo debe tener mas de 1 elemento')
 
+    return console.info({
+        original:arr,
+        sinDuplicados: arr.filter((value,index,self)=>self.indexOf(value) ===index)
+    })
+}
+qutarDuplicados(["x", 10, "x", 2, "10", 10, true, true])
+
+//E25S2 SET
+const quitarDuplicados = (arr=undefined) => {
+    if (arr===undefined) return console.warn('No ingesaste un arreglo')
+    if (!(arr instanceof Array)) return console.error('El valos que ingresaste no es un arreglo')
+    if (arr.length===0) return console.error('No puedes ingresar un arreglo vacio')
+    if (arr.length===1) return console.error('el arreglo debe tener mas de 1 elemento')
+
+    return console.info({
+        original:arr,
+        sinDuplicados: [...new Set(arr)]
+    })
+}
+quitarDuplicados(["x", 10, "x", 2, "10", 10, true, true])
 
 //--------------------------------------------------------------------------------------------
 
@@ -28,3 +85,34 @@ console.log("Ejercicio 25:")
 console.log("-----------------------------------------------------------------------------------")
 console.log("Ejercicio 26:")
 
+const arrProm = (arr) => {
+    let sum=0
+    arr.forEach(element => {
+        sum +=element
+    });
+    console.log(sum/arr.length)
+}
+arrProm([2,3,4,5,6])
+
+//another solutions validations and more efficient with reduce()
+
+const promedio = (arr=undefined) =>{
+    if (arr===undefined) return console.warn('No ingesaste un arreglo')
+    if (!(arr instanceof Array)) return console.error('El valos que ingresaste no es un arreglo')
+    if (arr.length===0) return console.error('No puedes ingresar un arreglo vacio')
+    for (let num of arr) {
+        if(typeof num !== "number") return console.error('El arreglos solo puede contener numeros')
+    }
+
+    return console.info(
+        arr.reduce((total,num,index,arr)=>{
+            total +=num
+            if(index ===arr.length-1){
+                return `El promedio de ${arr.join(" + ")} es ${total/arr.length}`
+            }else {
+                return total;
+            }
+        })
+    )
+}
+promedio([2,3,5,6,7,8,9,10])
